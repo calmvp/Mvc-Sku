76a3c28a5d9e65c11e37a3b0f4900277041a2cc4
Ned <nkaidayip@gmail.com>
2013-10-12 15:33:06 -0400

    fixed edit and delete navigation added cans controller

--- MvcSku/Controllers/CanController.cs
+++ MvcSku/Controllers/CanController.cs
@@ -10,12 +10,12 @@ using MvcSku.DAL;
 
 namespace MvcSku.Controllers
 {
-    public class UnitController : Controller
+    public class CanController : Controller
     {
         private LibraryContext db = new LibraryContext();
 
         //
-        // GET: /Unit/
+        // GET: /Can/
 
         public ActionResult Index()
         {
@@ -23,101 +23,96 @@ namespace MvcSku.Controllers
         }
 
         //
-        // GET: /Unit/Details/5
+        // GET: /Can/Details/5
 
         public ActionResult Details(int id = 0)
         {
-            Unit unit = db.Units.Find(id);
-            if (unit == null)
+            Can can = (Can)db.Units.Find(id);
+            if (can == null)
             {
                 return HttpNotFound();
             }
-            return View(unit);
+            return View(can);
         }
 
         //
-        // GET: /Unit/Create
+        // GET: /Can/Create
 
         public ActionResult Create()
         {
-            
-            // do we know the manufacturer_id?
-            // we need to pass it into the view
-            ViewBag.Manufacturer = db.Manufacturers.Find(Int32.Parse(Request.QueryString["ManufacturerId"]));
             return View();
         }
 
         //
-        // POST: /Unit/Create
+        // POST: /Can/Create
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Unit unit)
-        {
-            unit.Manufacturer = db.Manufacturers.Find(Int32.Parse(Request.QueryString["ManufacturerId"]));
-
-         
-                db.Units.Add(unit);
-                db.SaveChanges();
-
-
-                return RedirectToAction("Details", new { id = unit.UnitId } );
-        }
-
-        //
-        // GET: /Unit/Edit/5
-
-        public ActionResult Edit(int id = 0)
-        {
-            Unit unit = db.Units.Find(id);
-            if (unit == null)
-            {
-                return HttpNotFound();
-            }
-            return View(unit);
-        }
-
-        //
-        // POST: /Unit/Edit/5
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Unit unit)
+        public ActionResult Create(Can can)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(unit).State = EntityState.Modified;
+                db.Units.Add(can);
                 db.SaveChanges();
-                return RedirectToAction("Details", new { id = unit.UnitId });
+                return RedirectToAction("Index");
             }
-            return View(unit);
+
+            return View(can);
         }
 
         //
-        // GET: /Unit/Delete/5
+        // GET: /Can/Edit/5
 
-        public ActionResult Delete(int id = 0)
+        public ActionResult Edit(int id = 0)
         {
-            Unit unit = db.Units.Find(id);
-            if (unit == null)
+            Can can = (Can)db.Units.Find(id);
+            if (can == null)
             {
                 return HttpNotFound();
             }
-            return View(unit);
+            return View(can);
         }
 
         //
-        // POST: /Unit/Delete/5
+        // POST: /Can/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Can can)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(can).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(can);
+        }
+
+        //
+        // GET: /Can/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Can can = (Can)db.Units.Find(id);
+            if (can == null)
+            {
+                return HttpNotFound();
+            }
+            return View(can);
+        }
+
+        //
+        // POST: /Can/Delete/5
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Unit unit = db.Units.Find(id);
-            var ManufacturerInt = unit.Manufacturer.ManufacturerId;
-            db.Units.Remove(unit);
+            Can can = (Can)db.Units.Find(id);
+            db.Units.Remove(can);
             db.SaveChanges();
-            return RedirectToAction("Details", "Manufacturer", new { id = ManufacturerInt });
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
