beac604f6fea01c9f2736d1fb3ba3c0771495e04
Ned <nkaidayip@gmail.com>
2013-10-13 23:13:16 -0400

    cleaned notes out of code

--- MvcSku/Controllers/ManufacturerController.cs
+++ MvcSku/Controllers/ManufacturerController.cs
@@ -92,7 +92,7 @@ namespace MvcSku.Controllers
             }
             catch (DataException /* dex */)
    {
-      //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+
       ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
    }
    return View(manufacturer);
@@ -129,7 +129,6 @@ namespace MvcSku.Controllers
             }
             catch (DataException /* dex */)
             {
-                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
             }
             return View(manufacturer);
